40201f79b753a8bfce92f861562e7faffd8f2693
jenniferpower <121428642@umail.ucc.ie>
2023-11-18 16:48:47 +0000

    test method for add() method

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class ColourTableTest {
+    @Test
+    public void testAddMethod(){
+        ColourTable colourTable = new ColourTable(4);
+        initialSize=colourTable.getSize();
+        int [] rgbColourToAdd={255,0,0};
+        colourTable.add(rgbColourToAdd);
+
+        assertEquals(initialSize +1, colourTable.getSize());
+        assertTrue(colourTable.contains(rgbColourToAdd));
+    }
 }
